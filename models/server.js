74e8067759446fc02ea8547e20296abd27b5af76
juanmarcelo5 <marcecinco@hotmail.com>
2022-07-25 23:13:01 -0400

    first commit

---
+++ models/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConection } = require('../database/config');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+        
+        this.pahts = {
+                auth: '/api/auth',
+        }
+    
+      
+        
+        //conectar DB
+        this.conectarDb();
+        
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+    
+    async conectarDb(){
+        await dbConection();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+
+
+    }
+
+    routes() {
+        this.app.use( this.pahts.auth, require('../routes/auth'));
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const router = Router();
+
+router.post('/login',(req,res)=>{
+res.json({
+  msg:'Todo ok post'
+})
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
